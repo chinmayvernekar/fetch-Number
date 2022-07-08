b7a5c3c558196141f06b02746f837ce83c614ba6
chinmayvernekar <chinmay.vernekar49@gmail.com>
2022-07-08 10:35:25 +0530

    CatogeryCode_Model_Impl

---
+++ src/main/java/com/chinmay_vernekar/fetchnumbertask/CategoryCodeModel.java
@@ -0,0 +1,15 @@
+package com.chinmay_vernekar.fetchnumbertask;
+
+public class CategoryCodeModel {
+
+    private Integer categoryCode;
+
+    public Integer getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(Integer categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
+

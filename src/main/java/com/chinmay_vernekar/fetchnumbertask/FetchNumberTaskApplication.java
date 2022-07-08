003cd994eb6b8918e65140196ea4fd43821c8cf5
chinmayvernekar <chinmay.vernekar49@gmail.com>
2022-07-08 10:22:23 +0530

    initial setup

---
+++ src/main/java/com/chinmay_vernekar/fetchnumbertask/FetchNumberTaskApplication.java
@@ -0,0 +1,13 @@
+package com.chinmay_vernekar.fetchnumbertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchNumberTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchNumberTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chinmay_vernekar/fetchnumbertask/FetchNumberTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinmay_vernekar.fetchnumbertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchNumberTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4812a813bdec379ac5242a8dbb98bb8487903fed
chinmayvernekar <chinmay.vernekar49@gmail.com>
2022-07-08 10:43:51 +0530

    Query to find by CategoryCode Impl

--- src/main/java/com/chinmay_vernekar/fetchnumbertask/NumberRepo.java
+++ src/main/java/com/chinmay_vernekar/fetchnumbertask/NumberRepo.java
@@ -3,4 +3,6 @@ package com.chinmay_vernekar.fetchnumbertask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NumberRepo extends JpaRepository<NumberModel,Integer> {
+
+    NumberModel findByCategoryCode(Integer categoryCode);
 }
